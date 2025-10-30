e2fab79aabeb884ac8ff50035c2bcfbe137a4e5b
svniksv <svniksv@gmail.com>
2025-10-31 01:41:17 +0300

    Добавьте файлы проекта.

---
+++ run2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+
+    static List<string> Solve(List<(string, string)> edges)
+    {
+        var result = new List<string>();
+
+        Graph graph = new Graph(edges);
+        bool first = true;
+
+        while (!graph.IsIsolated())
+        {
+            var currentPath = BFS(graph, graph.Virus);
+            if (!first) graph.Virus = currentPath.Last().Item1;
+            else first = false;
+            result.Add(graph.RemoveConnection(currentPath[0]));
+        }
+
+        return result;
+    }
+
+    static List<(int, int)> BFS(Graph graph, int start)
+    {
+        var visited = new HashSet<int>();
+        var queue = new Queue<int>();
+        var mainPath = new List<(int, int)>();
+
+        queue.Enqueue(start);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue(); // берем элемент из очереди и помечаем его посещенным 
+            visited.Add(current);
+
+            List<int> connectedNodes = graph.GetConnectedNodes(current); // получаем всех соседей узла
+
+            foreach (var connection in connectedNodes)
+            {
+                if (visited.Contains(connection)) continue; // пропускаем исследованных соседей
+
+                queue.Enqueue(connection); //добавляем соседей в очередь
+                mainPath.Add((current, connection)); // сохраняем путь
+            }
+        }
+
+        //ищем путь до ближайшего шлюза
+        var shortestPath = new List<(int, int)>();
+        int end = graph.Nodes.Length;
+        foreach (var p in mainPath)
+        {
+            if (graph.Nodes[p.Item2].All(char.IsUpper))
+            {
+                if (p.Item2 < end) end = p.Item2;
+            }
+            else if (end != graph.Nodes.Length) break;
+        }
+
+        while (end != start)
+        {
+            foreach (var p in mainPath)
+            {
+                if (p.Item2 == end)
+                {
+                    shortestPath.Add((p.Item2, p.Item1));
+                    end = p.Item1;
+                    break;
+                }
+            }
+        }
+
+        return shortestPath;
+    }
+
+    static void Main()
+    {
+        var edges = new List<(string, string)>();
+        //string line;
+
+        //while ((line = Console.ReadLine()) != null)
+        //{
+        //    line = line.Trim();
+        //    if (!string.IsNullOrEmpty(line))
+        //    {
+        //        var parts = line.Split('-');
+        //        if (parts.Length == 2)
+        //        {
+        //            edges.Add((parts[0], parts[1]));
+        //        }
+        //    }
+        //} 
+        edges.Add(("a", "b"));
+        edges.Add(("b", "c"));
+        edges.Add(("c", "d"));
+        edges.Add(("c", "e"));
+        edges.Add(("A", "d"));
+        edges.Add(("A", "e"));
+        edges.Add(("c", "f"));
+        edges.Add(("c", "g"));
+        edges.Add(("f", "B"));
+        edges.Add(("g", "B"));
+
+
+        var result = Solve(edges);
+        foreach (var edge in result)
+        {
+            Console.WriteLine(edge);
+        }
+    }
+}
+
+public class Graph
+{
+    public int[,] GraphMatrix;
+    public string[] Nodes;
+    public int Virus;
+
+    public Graph(List<(string, string)> edges)
+    {
+        Nodes = CreateNodeList(edges);
+        GraphMatrix = new int[Nodes.Length, Nodes.Length];
+        Virus = 0;
+        FillingGraph(edges);
+    }
+
+    private void FillingGraph(List<(string, string)> edges)
+    {
+        foreach (var edge in edges)
+            AddConnection(edge);
+    }
+
+    private void AddConnection((string, string) edges)
+    {
+        int i = Array.IndexOf(Nodes, edges.Item1);
+        int j = Array.IndexOf(Nodes, edges.Item2);
+        GraphMatrix[i, j] = GraphMatrix[j, i] = 1;
+    }
+
+    public string RemoveConnection((int, int) edges)
+    {
+        GraphMatrix[edges.Item1, edges.Item2] = GraphMatrix[edges.Item2, edges.Item1] = 0;
+        return (Nodes[edges.Item1] + "-" +  Nodes[edges.Item2]);
+    }
+
+    public bool IsIsolated()
+    {
+        for (int i = 0; i < Nodes.Length; i++)
+        {
+            if (Nodes[i].All(char.IsUpper))
+            {
+                for (int j = 0; j < Nodes.Length; j++)
+                {
+                    if (GraphMatrix[i, j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public List<int> GetConnectedNodes(int index)
+    {
+        List<int> connections = new List<int>();
+
+        for (int i = 0;i < Nodes.Length;i++)
+            if (GraphMatrix[index,i] != 0) connections.Add(i);
+        return connections;
+    }
+
+    private string[] CreateNodeList(List<(string,string)> nodes)
+    {
+        List<string> knots = new List<string>();
+        List<string> gateway = new List<string>();
+
+        foreach (var item in nodes)
+        {
+            if (!(knots.Contains(item.Item1) || gateway.Contains(item.Item1)))
+            {
+                if (item.Item1.All(char.IsLower)) knots.Add(item.Item1);
+                else gateway.Add(item.Item1);
+            }
+            if (!(knots.Contains(item.Item2) || gateway.Contains(item.Item2)))
+            {
+                if (item.Item2.All(char.IsLower)) knots.Add(item.Item2);
+                else gateway.Add(item.Item2);
+            }
+        }
+
+        knots.Sort();
+        gateway.Sort();
+
+        List<string> totalNodes = knots.Concat(gateway).ToList();
+
+        string[] result = new string[totalNodes.Count];
+        for (int i = 0; i < totalNodes.Count; i++) result[i] = totalNodes[i];
+        return result;
+    }
+}
